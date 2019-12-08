f93fb8b5f37b92e653150e50895729617617e400
sreyleng.chheang <sreyleng.chheang2020@gmail.com>
2019-12-08 18:05:29 +0700

    update my project

--- js/project.js
+++ js/project.js
@@ -3,42 +3,109 @@ var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.js
 $(document).ready(function () {
     API(url);
 });
+//request data from api using arrow fucntion
 var API = (api) => {
     $.ajax({
         dataType: 'json',
         url: url,
-        success: (data) => getRecipse(data),
+        success: (data) => getDataRecipe(data),
         error: () => console.error("Cannot request data"),
     })
 }
-function getRecipse(datas) {
+//get data from api
+function getDataRecipe(datas) {
     datas.recipes.forEach(element => {
-        getElents(element);
+        getValueSelection(element);
     });
 }
-var getElents = (element) => {
-    getIdFromSelection(element);
-}
-function getIdFromSelection(element) {
+// get value from html select
+function getValueSelection(element) {
     $("#choose_my_option").on('change', function () {
         selection = $("#choose_my_option").val();
         if (element.id == selection) {
-            getElementById(element);
+            getValueApi(element);
             element.ingredients.forEach(item => {
-                ingradiants(item);
+                increasAndDescreasment(item);
+                var letter = "";
+                letter += `
+                    <h3 class="text-center">Ingrediants</h3>
+                `
+                ingrediants(item);
+                $('#text').html(letter);
             });
         }
     })
 }
-function getElementById(recipse) {
+//get name and icon url 
+function getValueApi(recipse) {
+    //distructuring
     const { name, iconUrl, nbGuests, instructions } = recipse;
     console.log(name);
-    //do
+    var recipes = "";
+    recipes += `
+        <div class="card shadow-lg">
+            <div class="card-header">${name}</div>
+            <div class="card-body"><img src="${iconUrl}" class="img-fluid"></div>
+           
+        </div>
+    `
+    $('#result').html(recipes);
 }
-function ingradiants(item) {
-   console.log(item.name);
-   //do
+//get ingrediants
+function ingrediants(item) {
+    const { name, quantity, unit, iconUrl } = item;
+    var table = "";
+    table += `
+        <tr>
+            <td>${name}</td>
+            <td>${quantity}</td>
+            <td>${unit[0]}</td>
+            <td><img src="${iconUrl}" class="img-fluid" width="100"></td>
+        </tr>
+    `
+    $('#ingredient').append(table);
 }
+//for increasment
+function increasAndDescreasment(increas) {
+    var input = "";
+    input += `
+         <div class="input-group mb-3">
+            <div class="input-group-append">
+                <button class="btn btn-danger" type="butdton" id="minus">&minus;</button>
+            </div>
+            
+                <input type="text" class="form-control" value ="0" id="value">
+
+            <div class="input-group-append">
+                <button class="btn btn-success" type="butdton" id="add">&plus;</button>
+            </div>
+         </div>
+                `
+
+    $('#increasment').html(input);
+   
+}
+
+// function minus(number) {
+//    $('#minus').on('click',function(){
+//     var min = $('#minus').val();
+//     descreas(min);
+//    });
+// }
+// function descreas(descreasment){
+//     var numbers = parseInt(descreasment)-1;
+//         return numbers;
+// }
+// function add(number) {
+//    $('#add').on('click',function(){
+//     var max = $('#minus').val();
+//     increas(max);
+//    });
+// }
+// function increas(increasment){
+//     var addNumber = parseInt(descreasment)+1;
+//         return addNumber;
+// }
 
 
 
